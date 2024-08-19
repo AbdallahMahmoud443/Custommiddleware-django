970a2b29ad049cce53544691c0d100dd8b17b2db
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2024-08-19 17:14:08 +0300

    add example of weekend in middleware done

--- custommiddlewareapp/middleware.py
+++ custommiddlewareapp/middleware.py
@@ -10,8 +10,11 @@ class MaintainMiddleware:
         self.get_response = get_response # get_response => reponsible for move from middleware to another
     
     def __call__(self,request): # call method => Execute it's code before access view 
-        if self.is_public_holiday():
-            return HttpResponseForbidden('Site is not accessiable in the public holiday')
+        # if self.is_public_holiday():
+        #     return HttpResponseForbidden('Site is not accessiable in the public holiday')
+        
+        if self.is_weekend():
+            return HttpResponseForbidden('Site is not accessiable in the WeekEnd')
         #! two lines of code below is important to continue next middleware in out project 
         #! Code Before Two line exceute before access view
         respone = self.get_response(request) 
@@ -19,6 +22,9 @@ class MaintainMiddleware:
         #! code After Two line exceute After access view
         
     
-    def is_public_holiday(self):
-        today = datetime.now().date()
-        return PublicHoliday.objects.filter(date=today).exists() #  exists() => return boolean variable@
+    # def is_public_holiday(self):
+    #     today = datetime.now().date()
+    #     return PublicHoliday.objects.filter(date=today).exists() #  exists() => return boolean variable
+    def is_weekend(self):
+        today = datetime.now().date().weekday()
+        return today in [0,1]
