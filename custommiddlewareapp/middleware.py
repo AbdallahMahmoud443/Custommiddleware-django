5a032f87fa58cbe6ca329871d228fbc783baef53
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2024-08-19 17:19:20 +0300

    custommiddleware done

--- custommiddlewareapp/middleware.py
+++ custommiddlewareapp/middleware.py
@@ -13,8 +13,12 @@ class MaintainMiddleware:
         # if self.is_public_holiday():
         #     return HttpResponseForbidden('Site is not accessiable in the public holiday')
         
-        if self.is_weekend():
-            return HttpResponseForbidden('Site is not accessiable in the WeekEnd')
+        # if self.is_weekend():
+        #     return HttpResponseForbidden('Site is not accessiable in the WeekEnd')
+        
+        if self.is_maintenance_time():
+            return HttpResponseForbidden('Site is  maintenance, Try Again After period of time')
+        
         #! two lines of code below is important to continue next middleware in out project 
         #! Code Before Two line exceute before access view
         respone = self.get_response(request) 
@@ -25,6 +29,11 @@ class MaintainMiddleware:
     # def is_public_holiday(self):
     #     today = datetime.now().date()
     #     return PublicHoliday.objects.filter(date=today).exists() #  exists() => return boolean variable
-    def is_weekend(self):
-        today = datetime.now().date().weekday()
-        return today in [0,1]
+    
+    # def is_weekend(self):
+    #     today = datetime.now().date().weekday()
+    #     return today in [0,1]
+    
+    def is_maintenance_time(self):
+        currentTime = datetime.now().hour
+        return (currentTime >= 17 and currentTime <= 20) # 17 => 5PM 20=> 8PM
